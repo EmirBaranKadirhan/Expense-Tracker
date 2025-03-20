c1d1e6d83cbfa7728dc25f70818f9087ee924c6a
Emir Baran Kadirhan <ebk1013@gmail.com>
2025-03-20 03:23:36 +0300

    continued to create login screen

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -10,25 +10,28 @@ import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
 import LockIcon from '@mui/icons-material/Lock';
+import Divider from '@mui/material/Divider';
+import Button from '@mui/material/Button';
+import ImageIcon from '@mui/icons-material/Image';
 
 function Login() {
     return (
-        <div>
+        <div style={{ display: 'flex', height: '100vh', width: '100%', alignItems: 'center' }}>
             <Box
                 sx={{
                     display: 'flex',
-                    flexWrap: 'wrap',
-                    '& > :not(style)': {
-                        m: 1,
-                        width: 400,
-                        height: 400,
-                    },
+                    alignItems: 'center',
+                    flexGrow: 1,            // Her iki tarafın eşit genişlikte olmasını sağlar
+                    p: 2,
+                    justifyContent: 'flex-end',
+                    marginRight: '75px',
+                    width: '700px',
+                    height: '700px'
                 }}
             >
 
-
-                <Paper elevation={5}>
-                    <Typography variant="h5" component="h1" gutterBottom>
+                <Paper elevation={5} style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly', width: '55%', height: '60%' }}>
+                    <Typography variant="h5" component="h1" gutterBottom sx={{ textAlign: 'center' }} >
                         Kullanıcı Kayıt
                     </Typography>
                     <FormControl variant="standard" style={{ width: '95%' }}>
@@ -71,9 +74,38 @@ function Login() {
                             variant="outlined"
                         />
                     </FormControl>
+                    <FormControl variant="standard" style={{ width: '95%' }}>
+                        <TextField
+                            type='file'                             // resim ya da dosya eklemek icin 
+                            label="Profil Fotografi Ekleyiniz"
+                            id="filled-start-adornment"
+                            sx={{ m: 1, width: '100%' }}
+                            slotProps={{
+                                input: {
+                                    startAdornment: <InputAdornment position="start"><ImageIcon /></InputAdornment>,
+                                },
+                            }}
+                            variant="outlined"
+                        />
+                    </FormControl>
+                    <Box sx={{ display: 'flex' }}>
+                        <Button sx={{ width: '200px', marginRight: '58px', marginLeft: '8px' }} variant="contained">Kayit Ol</Button>
+                        <Button color='success' sx={{ width: '200px' }} variant="contained">Giris Yap</Button>
+                    </Box>
+
                 </Paper>
             </Box>
-        </div>
+            <Divider orientation="vertical" flexItem />
+            <Box sx={{
+                display: 'flex;', alignItems: 'center;', justifyContent: 'flex-start', flexGrow: 1,
+                p: 2, marginLeft: '75px'
+            }}>
+                <Box sx={{ width: '650px', height: '650px' }}>
+                    <img src="https://cdn-icons-png.flaticon.com/512/8910/8910710.png" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+                </Box>
+            </Box>
+
+        </div >
     )
 }
 
