4d4afd7b95fbfe705dbe9d9466403fd7ccbb1580
Emir Baran Kadirhan <ebk1013@gmail.com>
2025-03-25 17:56:03 +0300

    new income type category added

--- src/components/GelirHesaplama.jsx
+++ src/components/GelirHesaplama.jsx
@@ -11,36 +11,42 @@ import Button from '@mui/material/Button';
 import { useDispatch, useSelector } from 'react-redux'
 import { addIncomes, sendToFirestore } from '../redux/slices/transactionsSlice'
 import { auth } from '../firebase/firebaseConfig'
+import DialogTitle from '@mui/material/DialogTitle';
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
 
-
-const currencies = [
-    {
-        value: 'web sitesi yapimi',
-        label: 'Web Sitesi Yapimi',
-    },
-    {
-        value: 'seo calismasi',
-        label: 'SEO Calismasi',
-    },
-    {
-        value: 'diger gelirler',
-        label: 'Diger Gelirler',
-    }
-];
 
 
 function GelirHesaplama() {
 
     const [amount, setAmount] = useState("");
     const [category, setCategory] = useState("");
+    const [newCategory, setNewCategory] = useState("");
+    const [categories, setCategories] = useState([
+        {
+            value: 'web sitesi yapimi',
+            label: 'Web Sitesi Yapimi',
+        },
+        {
+            value: 'seo calismasi',
+            label: 'SEO Calismasi',
+        },
+        {
+            value: 'diger gelirler',
+            label: 'Diger Gelirler',
+        }
+    ]);
+
     const [description, setDescription] = useState("");
+    const [openDialog, setOpenDialog] = useState(false);
 
     const incomes = useSelector((state) => state.transactions.incomes)
     console.log(incomes);
     const dispatch = useDispatch();
 
     const handleSubmit = () => {
-        if (amount) {
+        if (amount && category) {
             const userId = auth.currentUser.uid;    // auth.currentUser => su anda oturum acmis olan kullanciyi temsil eder
             const income = { type: "Gelir", amount: Number(amount), category, description, date: new Date().toLocaleDateString('tr-TR') };
             dispatch(addIncomes(income));
@@ -49,6 +55,19 @@ function GelirHesaplama() {
             setCategory("");
             setDescription("");
 
+
+        }
+    }
+
+    const handleAddCategory = () => {
+        if (newCategory.trim() !== '') {
+            const formattedCategory = {
+                value: newCategory,
+                label: newCategory
+            }
+            setCategories([...categories, formattedCategory]);   // kategoriler listesine yeni kategoriyi ekledik
+            setNewCategory("");
+            setOpenDialog(false);
 
         }
     }
@@ -78,7 +97,7 @@ function GelirHesaplama() {
                     onChange={(e) => setCategory(e.target.value)}
                     value={category}
                 >
-                    {currencies.map((option) => (
+                    {categories.map((option) => (
                         <MenuItem key={option.value} value={option.value}>
                             {option.label}
                         </MenuItem>
@@ -96,8 +115,28 @@ function GelirHesaplama() {
                     value={description}
                 />
                 <Button onClick={handleSubmit} color='success' variant="contained" sx={{ marginTop: '20px' }}>Gelir Ekle</Button>
+                <Button onClick={() => setOpenDialog(true)} variant="outlined" sx={{ marginTop: '20px' }}>Gelir Türü Ekle</Button>
             </Box>
-        </div>
+            <Dialog open={openDialog} onClose={() => setOpenDialog(false)}>
+                <DialogTitle>Yeni Kategori Ekle</DialogTitle>
+                <DialogContent>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        label="Kategori Adı"
+                        fullWidth
+                        variant="standard"
+                        onChange={(e) => setNewCategory(e.target.value)}
+                    // value={ }
+
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={() => setOpenDialog(false)}>İptal</Button>
+                    <Button onClick={handleAddCategory}>Ekle</Button>
+                </DialogActions>
+            </Dialog>
+        </div >
     )
 }
 
